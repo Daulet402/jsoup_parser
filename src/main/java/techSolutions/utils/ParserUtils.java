fbae62432e5d27b08d33140db28918f9f4895891
daulet <daulet031@gmail.com>
2017-08-27 14:40:29 +0600

    First commit of Parser App

---
+++ src/main/java/techSolutions/utils/ParserUtils.java
@@ -0,0 +1,29 @@
+package techSolutions.utils;
+
+import org.jsoup.nodes.Element;
+
+import java.util.Objects;
+
+/**
+ * daulet  8/27/17
+ */
+public class ParserUtils {
+
+    public static String getAttrValue(Element element, String attrKey) {
+        return Objects.nonNull(element) ? element.attr(attrKey) : null;
+    }
+
+    public static Element getElementByTags(Element element, String... tags) {
+        Element el = element;
+        for (String tag : tags) {
+            if (Objects.nonNull(el)) {
+                el = el.getElementsByTag(tag).first();
+            }
+        }
+        return el;
+    }
+
+    public static String getText(Element element) {
+        return Objects.nonNull(element) ? element.text() : null;
+    }
+}
